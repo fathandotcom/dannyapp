86f8da5689798f0d21b50b3b34a2da5119ec51a3
fathandotcom <fathanqinthar@gmail.com>
2025-06-02 19:57:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("danny siswanto")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "berat saya 150kg"
 )
